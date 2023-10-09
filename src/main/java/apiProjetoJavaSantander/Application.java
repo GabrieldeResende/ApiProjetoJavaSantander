7c8cdf6968dcfe2011244d380684598ce49dcd72
GabrieldeResende <109700756+GabrieldeResende@users.noreply.github.com>
2023-10-09 11:15:00 -0300

    criação do domain

--- src/main/java/apiProjetoJavaSantander/Application.java
+++ src/main/java/apiProjetoJavaSantander/Application.java
@@ -1,4 +1,4 @@
-package com.minhaApi.ApiProjetoJavaSantander;
+package apiProjetoJavaSantander;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args); 
 	}
 
 }
